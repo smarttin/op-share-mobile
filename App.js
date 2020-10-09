622ccb1fde57a856e4c44e0828dcee972d809f99
smarttin <smarttin77@gmail.com>
2020-10-09 12:44:01 +0100

    preload logo and fonts

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
+import {AppLoading} from 'expo';
+import * as Font from 'expo-font';
+import {Asset} from 'expo-asset';
+import {Ionicons} from '@expo/vector-icons';
+import {Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  const [loaded, setLoaded] = useState(false);
+
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({...Ionicons.font});
+      await Asset.loadAsync([require('./assets/logo.png')]);
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    preLoad();
+    return (cleanUp = () => {});
+  }, []);
+
+  return loaded ? (
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
